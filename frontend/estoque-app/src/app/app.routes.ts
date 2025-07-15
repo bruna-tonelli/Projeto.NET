824f96b64f8f746eb61b1a10cfab7e67d7e3a4a7
bruna-tonelli <bc39081@gmail.com>
2025-07-15 14:06:07 -0300

    Update app.routes.ts

--- frontend/estoque-app/src/app/app.routes.ts
+++ frontend/estoque-app/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { EstoqueComponent } from './components/estoque/estoque.component';
 import { MovimentacaoComponent } from './Movimentacao/movimentacao.component';
 import { FuncionarioComponent } from './Funcionarios/funcionario.component';
-//import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AuthGuard } from './guards/auth.guard';
 import { FinanceiroComponent } from './Financeiro/financeiro.component';
@@ -10,7 +10,7 @@ import { InventarioComponent } from './Inventario/inventario.component';
 
 export const routes: Routes = [
   // Rotas de autenticação (não protegidas)
-  //{ path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 
   // Rota padrão: Se o usuário acessar a raiz, redirecione para /estoque
@@ -18,13 +18,13 @@ export const routes: Routes = [
 
 
   // Rotas protegidas com AuthGuard
-  { path: 'estoque', component: EstoqueComponent},
-  { path: 'movimentacao', component: MovimentacaoComponent},
-  { path: 'funcionarios', component: FuncionarioComponent,  },
-  { path: 'financeiro', component: FinanceiroComponent,  },
-  { path: 'inventario' , component: InventarioComponent,  },
+  { path: 'estoque', component: EstoqueComponent, canActivate: [AuthGuard] },
+  { path: 'movimentacao', component: MovimentacaoComponent, canActivate: [AuthGuard] },
+  { path: 'funcionarios', component: FuncionarioComponent, canActivate: [AuthGuard]  },
+  { path: 'financeiro', component: FinanceiroComponent, canActivate: [AuthGuard]  },
+  { path: 'inventario' , component: InventarioComponent, canActivate: [AuthGuard]  },
 
   
   // Rota catch-all: redireciona para login para rotas não encontradas
-  //{ path: '**', redirectTo: '/login' }
+  { path: '**', redirectTo: '/login' }
 ];
