69ad6ccec7a700187f6bce6ab6fe3c13a96bea7a
Vinicius Donovan <105085064+ViniciusDonovanC@users.noreply.github.com>
2025-07-15 12:22:19 -0300

    Microserviço Inventario
    Microserviço Inventario

---
+++ InventarioService/Models/Inventario.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InventarioService.Models
+{
+    public class Inventario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; } = string.Empty;
+
+        public string? Descricao { get; set; }
+
+        [Required]
+        public int Quantidade { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal PrecoVenda { get; set; }
+
+        public bool Confirmado { get; set; } = false;
+        public DateTime DataCadastro { get; set; } = DateTime.UtcNow;
+    }
+}
